f1f632eee702f9de19b270932bf70bbce00089c2
OCPik4chu <ocgreg.25@gmail.com>
2025-06-10 08:58:43 -0600

    PyQt6 POC completed, migration started

--- budgetbook/__main__.py
+++ budgetbook/__main__.py
@@ -3,7 +3,22 @@
 import utilities.handlers as handlers
 import utilities.db_handlers as db_handlers
 import os
+import sys
 
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QMainWindow,
+    QTabWidget,
+    QVBoxLayout,
+    QDateEdit,
+    QLabel,
+    QPushButton,
+    QLineEdit,
+)
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox, filedialog
@@ -19,95 +34,236 @@ except Exception:
 
 logger = LoggingHandler(str(os.path.basename(__file__))).log
 
-root = Tk()
-root.title("Welcome to MyPyBudget")
-root.geometry("1024x800")
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-root.configure(background="DarkGray")
 
-nb = ttk.Notebook(root, padding="8 8 12 12")
-mainframe = ttk.Frame(nb)
-mainframe.grid(column=0, columnspan=9, row=0, rowspan=9, sticky=(N, W, E, S))
+# converting tkinter window to class to handle multiple windows
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-nb.add(mainframe, text="Main")
-nb.pack()
+        self.setWindowTitle("Budget Book")
+        self.setMinimumSize(QSize(1000, 800))
 
-importFilePrompt = StringVar()
+        # Setup Tab Menus
+        self.main_tabs = QTabWidget()
+        self.main_tabs.setTabPosition(QTabWidget.TabPosition.North)
+        self.main_tabs.setMovable(False)
+        self.main_tabs.setStyleSheet(
+            """
+         QTabBar::tab {
+             font-size: 12pt;
+         }
+     """
+        )
+
+        # Summary Tab
+        summary_tab = QWidget()
+        summary_layout = QVBoxLayout()
+        summary_label = QLabel("Summary")
+        summary_layout.addWidget(summary_label)
+        summary_layout.addStretch()
+        summary_tab.setLayout(summary_layout)
+        self.main_tabs.addTab(summary_tab, "Summary")
+
+        # Report Tab
+        report_tab = QWidget()
+        report_layout = QVBoxLayout()
+        report_label = QLabel("Reports")
+        report_layout.addWidget(report_label)
+        report_layout.addStretch()
+        report_tab.setLayout(report_layout)
+        self.main_tabs.addTab(report_tab, "Reports")
+
+        # Imports Tab
+        imports_tab = QWidget()
+        imports_layout = QVBoxLayout()
+        imports_label = QLabel("Imports")
+        imports_table = handlers.TableData()
+        imports_layout.addWidget(imports_label)
+        imports_layout.addWidget(imports_table)
+        imports_layout.addStretch()
+        imports_tab.setLayout(imports_layout)
+        self.main_tabs.addTab(imports_tab, "Imports")
+
+        # Report Tab
+        updates_tab = QWidget()
+        updates_layout = QVBoxLayout()
+        updates_label = QLabel("Updates")
+        updates_layout.addWidget(updates_label)
+        updates_layout.addStretch()
+        updates_tab.setLayout(updates_layout)
+        self.main_tabs.addTab(updates_tab, "Updates")
+
+        self.setCentralWidget(self.main_tabs)
+        # Setup Main Buttons
+
+        # view_expenses_button = QPushButton(text="View Expense Summar", parent=self)
+        # view_expenses_button.setFixedSize(200, 20)
+        # budget_report_button = QPushButton(text="View Budget Report", parent=self)
+        # budget_report_button.setFixedSize(200, 20)
+        # print_expenses_button = QPushButton(text="Print Expenses", parent=self)
+        # print_expenses_button.setFixedSize(200, 20)
+        # import_expenses_button = QPushButton(text="Import Expenses", parent=self)
+        # import_expenses_button.setFixedSize(200, 20)
+        # save_expenses_button = QPushButton(text="Save Expenses", parent=self)
+        # save_expenses_button.setFixedSize(200, 20)
+        # delete_duplicates_button = QPushButton(text="delete duplicates", parent=self)
+        # delete_duplicates_button.setFixedSize(200, 20)
+        # quit_button = QPushButton(text="Quit", parent=self)
+        # quit_button.setFixedSize(200, 20)
+
+        # main_window_layout = QVBoxLayout()
+
+        # main_window_layout.addWidget(view_expenses_button)
+        # main_window_layout.addWidget(budget_report_button)
+        # main_window_layout.addWidget(print_expenses_button)
+        # main_window_layout.addWidget(import_expenses_button)
+        # main_window_layout.addWidget(save_expenses_button)
+        # main_window_layout.addWidget(delete_duplicates_button)
+        # main_window_layout.addWidget(quit_button)
+        # main_window_layout.addWidget(main_tabs)
+        # main_window_widget = QWidget()
+
+        # main_window_widget.setLayout(main_window_layout)
+        # self.setCentralWidget(main_window_widget)
 
 
-def importFile():  # This works now, don't change it
-    importFilePrompt.set(filedialog.askopenfilename(title="Select Expenses to Import"))
-    importFile = importFilePrompt.get()
-    if importFile == "":
-        return
-    else:
-        expenses, rowcount = handlers.csvImporter(importFile)
-        # Passes processed row count and expense list to update textboxes
-        BoxMaking.updateTextBox(expenses, rowcount)
+# class MainWindow(ttk.Frame):
+#     def __init__(self, master=None, **kwargs):
+#         super().__init__(master, **kwargs)
+
+#         ttk.Button(self, text="View Expense Summary", command="").grid(
+#             column=8, row=2, sticky=(E, S)
+#         )
+#         ttk.Button(self, text="View Budget Report", command="").grid(
+#             column=8, row=3, sticky=(E, S)
+#         )
+#         ttk.Button(self, text="Print Expenses", command=self.printBoxContents).grid(
+#             column=8, row=4, sticky=(E, S)
+#         )
+#         ttk.Button(self, text="Import Expenses", command=self.openFileImporter).grid(
+#             column=7, row=5, sticky=(E, S)
+#         )
+#         ttk.Button(self, text="Save Expenses", command=self.saveExpenses).grid(
+#             column=8, row=5, sticky=(E, S)
+#         )
+#         ttk.Button(self, text="Quit", command=self.leave).grid(
+#             column=8, row=6, sticky=(E, S)
+#         )
+#         ttk.Button(self, text="delete duplicates", command=self.deleteDupes).grid(
+#             column=7, row=7, sticky=(E, S), padx=5, pady=10
+#         )
+#         # ttk.Button(mainframe, text="Print Box content", command=lambda: self.BoxMaking.print_box_data()).grid(column=8, row=8, padx=5, pady=5)
+#         self.BoxMaking = handlers.EntryBoxBuilder(self)
+#         self.importFilePrompt = StringVar()
+
+#     def saveExpenses(self):
+#         expenses = self.BoxMaking.getBoxContents()
+#         handlers.writeExpenseToDB(expenses)
+
+#     def printBoxContents(self):
+#         try:
+#             print(self.BoxMaking.getBoxContents())
+#             # self.BoxMaking.print_box_data()
+#         except AttributeError:
+#             messagebox.showwarning(message="Error: No data has been loaded yet")
+#             # print("Error: No data has been loaded yet")
+
+#     def openFileImporter(self):
+
+#         try:
+#             expenses, rowcount = FileImportWindow(self)
+#         except TypeError:
+#             # If the import is cancelled or contains no data set empty values
+#             expenses, rowcount = "", ""
+#         if expenses or rowcount == "":
+#             # If there is no data or if the user cancelled then do nothing
+#             return
+#         else:
+#             # the returned information from the popup should give back these items
+#             self.BoxMaking.updateTextBox(expenses, rowcount)
+
+#     def deleteDupes(self):
+#         db_handlers.removeDuplicates()
+
+#     def leave(self):
+#         try:
+#             quit()
+#         except Exception as e:
+#             print(f"I have no idea how this failed but it was because of: {e}")
 
 
-def saveExpenses():
-    expenses = BoxMaking.getBoxContents()
-    handlers.writeExpenseToDB(expenses)
+class FileImportWindow(Toplevel):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.title("Select budget file to import")
+        self.geometry("400x400")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.grid(column=0, columnspan=3, row=0, rowspan=3, sticky=(N, W, E, S))
+        self.yearChoice = ["2020", "2021", "2022", "2023", "2024", "2025"]
+        self.fileType = ["CSV", "PDF"]
+        self.importFilePrompt = StringVar()
+        self.fileTypeSelection = StringVar()
+        self.importYearSelection = StringVar()
+        # Drop down lists and button to choose import file go here
+        self.fileType = ttk.Combobox(
+            self, textvariable=self.fileTypeSelection, values=self.fileType
+        )
+        self.fileType.grid(column=1, row=1, sticky=(N, W))
+        self.importYear = ttk.Combobox(
+            self, self.importYearSelection, values=self.yearChoice
+        )
+        self.importYear.grid(column=2, row=1, sticky=(N, W))
 
+        # these are window controls to keep control to the popup dialogue window
 
-def printBoxContents():
-    try:
-        print(BoxMaking.getBoxContents())
-        # BoxMaking.print_box_data()
-    except AttributeError:
-        messagebox.showwarning(message="Error: No data has been loaded yet")
-        # print("Error: No data has been loaded yet")
+        self.transient(master)  # open on top of main window
+        self.grab_set()  # hijack all commands from master window
+        master.wait_window(self)  # pause anything on main window until this closes
 
-
-def deleteDupes():
-    db_handlers.removeDuplicates()
-
-
-def leave():
-    try:
-        quit()
-    except Exception as e:
-        print(f"I have no idea how this failed but it was because of: {e}")
-
-
-# Create instance of the class to build rows with
-BoxMaking = handlers.EntryBoxBuilder(mainframe)
+    def importFile(self):  # This works now, don't change it
+        self.importFilePrompt.set(
+            filedialog.askopenfilename(title="Select Expenses to Import")
+        )
+        importFile = self.importFilePrompt.get()
+        if importFile == "":
+            return
+        else:
+            expenses, rowcount = handlers.csvImporter(importFile)
+            # Passes processed row count and expense list to update textboxes
+            self.BoxMaking.updateTextBox(expenses, rowcount)
 
 
 def main():
+    # root = Tk()
+    # root.title("Welcome to MyPyBudget")
+    # root.geometry("1024x800")
+    # root.columnconfigure(0, weight=1)
+    # root.rowconfigure(0, weight=1)
 
-    # Moved all functions and setup outside main function.
+    # root.configure(background="DarkGray")
+    # style = ttk.Style(root)
+    # style.configure("TNotebook.Tab", width=root.winfo_screenwidth())
 
-    ttk.Button(mainframe, text="View Expense Summary", command="").grid(
-        column=8, row=2, sticky=(E, S)
-    )
-    ttk.Button(mainframe, text="View Budget Report", command="").grid(
-        column=8, row=3, sticky=(E, S)
-    )
-    ttk.Button(mainframe, text="Print Expenses", command=printBoxContents).grid(
-        column=8, row=4, sticky=(E, S)
-    )
-    ttk.Button(mainframe, text="Import Expenses", command=importFile).grid(
-        column=7, row=5, sticky=(E, S)
-    )
-    ttk.Button(mainframe, text="Save Expenses", command=saveExpenses).grid(
-        column=8, row=5, sticky=(E, S)
-    )
-    ttk.Button(mainframe, text="Quit", command=leave).grid(
-        column=8, row=6, sticky=(E, S)
-    )
-    # These were just for troubleshooting
-    # rowcount = ttk.Entry(mainframe, width=5)
-    # rowcount.grid(column=8, row=7, padx=10, pady=10)
-    ttk.Button(mainframe, text="delete duplicates", command=deleteDupes).grid(
-        column=7, row=7, sticky=(E, S), padx=5, pady=10
-    )
-    # ttk.Button(mainframe, text="Print Box content", command=lambda: BoxMaking.print_box_data()).grid(column=8, row=8, padx=5, pady=5)
+    # nb = ttk.Notebook(
+    #     root,
+    #     padding="8 8 12 12",
+    #     width=root.winfo_screenmmwidth(),
+    #     height=root.winfo_screenmmheight(),
+    # )
 
-    # TextBoxBuilder(root)
-    root.mainloop()
+    # # I need to check the formatting and layout, its broken now with the class
+    # mainframe = MainWindow(nb)
+    # mainframe.grid(column=0, columnspan=9, row=0, rowspan=9, sticky=(N, W, E, S))
+
+    # nb.add(mainframe, text="Main")
+    # nb.pack()
+    # root.mainloop()
+
+    main_app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(main_app.exec())
 
 
 if __name__ == "__main__":
--- budgetbook/utilities/handlers.py
+++ budgetbook/utilities/handlers.py
@@ -2,6 +2,10 @@
 # This file is a copy of handlers.py but is being modified for django integration and testing
 import os
 import csv
+from PyQt6.QtWidgets import QLineEdit
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import Qt
+
 # import itertools
 from tkinter import *
 from tkinter import ttk
@@ -11,8 +15,9 @@ from datetime import datetime
 import vars.settings as settings
 from utilities.logger import LoggingHandler
 import utilities.db_handlers as db_handlers
-   
-# try: 
+
+
+# try:
 #     import utilities.db_handlers as db_handlers
 # except Exception:
 #     import db_handlers as db_handlers
@@ -20,21 +25,63 @@ import utilities.db_handlers as db_handlers
 month_selector = settings.month_selector
 logger = LoggingHandler(str(os.path.basename(__file__))).log
 
-#the old TextBoxBuilder has been replaced with entry box handling because that is better.  The original POC is in its own file
+# the old TextBoxBuilder has been replaced with entry box handling because that is better.  The original POC is in its own file
+
+
+class TableDataDisplay(QtCore.QAbstractTableModel):
+    # https://www.pythonguis.com/tutorials/pyqt6-qtableview-modelviews-numpy-pandas/
+    def __init__(self, data):
+        super().__init__()
+        self.name = __name__
+        self.logger = LoggingHandler(__class__).log
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.ItemDataRole.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return len(self._data[0])
+
+
+class TableData(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.table = QtWidgets.QTableView()
+
+        data = [
+            [33, 18, 4, 9, 2],
+            [443, "lets test the dynamic box size for these things", 1, 0, 0],
+            [23, 58, 3, 5, 0],
+            [873, 28, 3, 3, 2],
+            [13, 8, 7, 8, 9],
+        ]
+        self.model = TableDataDisplay(data)
+        self.table.setModel(self.model)
+
+        self.setCentralWidget(self.table)
+
+
 class EntryBoxBuilder:
     # original test moved to TextBoxBuilder_test.py
-    
+
     def __init__(self, mainframe):
         self.name = __name__
         self.mainframe = mainframe
         self.logger = LoggingHandler(__class__).log
-        #stringVar not currently used nor seems to be needed?
+        # stringVar not currently used nor seems to be needed?
         self.dateBoxValue = StringVar()
         self.chargeBoxValue = StringVar()
         self.amountBoxValue = StringVar()
         self.tagBoxValue = StringVar()
         self.noteBoxValue = StringVar()
-        
 
     def createEntryBoxRow(self, rowNumber):
         # print(f"createEntryBoxRow was called with expensedata: {expenses} \n And rowNumber of: {rowNumber}")
@@ -47,7 +94,7 @@ class EntryBoxBuilder:
         noteBox = ttk.Entry(self.mainframe, width=35)
 
         for r in range(rowNumber):
-            rowSpacing = r+1
+            rowSpacing = r + 1
             # print(f"row number: {r} and rowcount var {rowNumber}")
             dateBox.grid(column=1, row=rowSpacing, sticky=N, padx=1)
             chargeBox.grid(column=2, row=rowSpacing, sticky=N, padx=1)
@@ -63,18 +110,22 @@ class EntryBoxBuilder:
 
     def createEntryBoxes(self, rowcount):
         try:
-            #this is the sidestep to using the input box for testing and isnt needed for now
+            # this is the sidestep to using the input box for testing and isnt needed for now
             totalrows = int(rowcount.get())
         except Exception:
             totalrows = rowcount
         self.GlobalBoxList = []
         for row in range(totalrows):
-            rowID = row +1
-            self.createEntryBoxRow(rowID)  # This works (forgot self.) and doesnt require input data
+            rowID = row + 1
+            self.createEntryBoxRow(
+                rowID
+            )  # This works (forgot self.) and doesnt require input data
             for o in self.BoxObjectList:
                 self.GlobalBoxList.append(o)
         # print(f"inside makeBoxes GlobalBoxList contains: {self.GlobalBoxList}")
-        self.logger.debug(f"inside makeBoxes GlobalBoxList contains: {self.GlobalBoxList}")
+        self.logger.debug(
+            f"inside makeBoxes GlobalBoxList contains: {self.GlobalBoxList}"
+        )
         # return self.BoxObjectList
         return self.GlobalBoxList
 
@@ -89,7 +140,7 @@ class EntryBoxBuilder:
         while i < len(expenseList):
             boxList[i].insert(0, expenseList[i])
             i += 1
-    
+
     def getBoxContents(self):
         self.GlobalExpenseData = []
         try:
@@ -100,7 +151,10 @@ class EntryBoxBuilder:
         except AttributeError:
             messagebox.showwarning(message="Error: No data has been loaded yet")
             return
+
+
 # End EntryBoxBiulder Class ================================================
+
 
 ####### Misc utilities  #######
 def dateCheck(datestring, fuzzy=False):
@@ -108,10 +162,12 @@ def dateCheck(datestring, fuzzy=False):
         dateparse(datestring, fuzzy=fuzzy)
         return True
     except Exception as e:
-        #e isnt used but its caught for proper coding, this merely needs to return false if it cant parse the date for any reason
+        # e isnt used but its caught for proper coding, this merely needs to return false if it cant parse the date for any reason
         return False
 
+
 ####### File Handlers ########
+
 
 def csvImporter(inputFileName, year="2025"):
     # Works perfectly!  results in a joined list of formatted data
@@ -119,7 +175,9 @@ def csvImporter(inputFileName, year="2025"):
     # For now we hardcode tab delim, need to do better handling later
     try:
         with open(inputFileName, newline="") as infile:
-            infilereader = csv.reader(filter(lambda line: line.strip(), infile), delimiter=",")        
+            infilereader = csv.reader(
+                filter(lambda line: line.strip(), infile), delimiter=","
+            )
             for row in infilereader:
                 parsedRow, rowCount = parseCSV(row, year)
                 for i in parsedRow:
@@ -129,19 +187,21 @@ def csvImporter(inputFileName, year="2025"):
     except FileNotFoundError:
         logger.critical("No valid file was found to inport")
 
+
 ####### Parsers and Writers ########
+
 
 def parseCSV(row, year):  # Works perfect!
     # New function to parse the csv one row at a time and reformat the data into a list of strings
     expenses = []
     i = 0
-    date, charge_name, expense, tag, notes = "","","","",""
+    date, charge_name, expense, tag, notes = "", "", "", "", ""
     while i < len(row):
         if row[i] != "":
             if dateCheck(row[i], fuzzy=False) is True:
                 date = "'{}'".format(row[i])
                 date = year + " " + str(date).strip("'")
-                date = "{}".format(str(datetime.strptime(date,"%Y %b %d").date()))
+                date = "{}".format(str(datetime.strptime(date, "%Y %b %d").date()))
                 expenses.append(date)
             elif "$" in row[i]:
                 expense = row[i].replace("$", "").strip("\n")
@@ -159,10 +219,12 @@ def parseCSV(row, year):  # Works perfect!
     return expenses, i
     # print(f"Espenses prior to return: {expenses}")
 
+
 def expenseChunks(expenseList, chunkSize):
     # this should take the list and return tuples of size chunkSize for all data sent to it
     for i in range(0, len(expenseList), chunkSize):
-        yield expenseList[i:i+chunkSize]
+        yield expenseList[i : i + chunkSize]
+
 
 def writeExpenseToDB(expenses):
     """
@@ -178,11 +240,12 @@ def writeExpenseToDB(expenses):
         messagebox.showinfo(message="All Entries saved to the Database")
         # db_handlers.addExpenses(expense_batch, "2024")
 
+
 def parseToSQL(rawimport, year="2025"):
-    #this was replaced by parseCSV, we need to create a new generic SQL prep function to write data to the DB
+    # this was replaced by parseCSV, we need to create a new generic SQL prep function to write data to the DB
     # parse input data to sql data and call writeToDB
     i = 0
-    date, charge_name, expense = "","",""
+    date, charge_name, expense = "", "", ""
     csvline = rawimport.split("  ")
     while i < len(csvline):
         if csvline[i] != "":
@@ -190,7 +253,7 @@ def parseToSQL(rawimport, year="2025"):
                 date = "'{}'".format(csvline[i])
                 date = year + " " + str(date).strip("'")
                 # Convert date to standard format for DB usage
-                date = "'{}'".format(str(datetime.strptime(date,"%Y %b %d").date()))
+                date = "'{}'".format(str(datetime.strptime(date, "%Y %b %d").date()))
                 # print(date)
             elif "$" in csvline[i]:
                 expense = csvline[i].replace("$", "")
@@ -199,21 +262,25 @@ def parseToSQL(rawimport, year="2025"):
             i = i + 1
         else:
             i = i + 1
-    #print(date + charge_name + expense)
+    # print(date + charge_name + expense)
     writeExpenseToDB(date, charge_name, expense)
 
-def monthlyQueryBuilder(): #rewriting for error handling on bad input
-    #not currently used in the UI
+
+def monthlyQueryBuilder():  # rewriting for error handling on bad input
+    # not currently used in the UI
     month = ""
     month = input("Enter the name of the Month to modify: ").capitalize()
     while month not in month_selector:
-        month = input("That was not a valid choice.  Month must be a name or abbrev, e.g. Mar or March: ").capitalize()
+        month = input(
+            "That was not a valid choice.  Month must be a name or abbrev, e.g. Mar or March: "
+        ).capitalize()
         month = "" + month[0:3]
         # print(month)
     else:
         print("looks fine, calling query")
         listByMonth = db_handlers.queryByMonth(month)
     return listByMonth
+
 
 # def monthlyQueryBuilder(): original, works ok
 #     month = input("Enter the name of the Month to modify: ").capitalize()
@@ -225,15 +292,18 @@ def monthlyQueryBuilder(): #rewriting for error handling on bad input
 #         listByMonth = db_handlers.queryByMonth(month)
 #     return listByMonth
 
+
 def chooseExpenseToTag():
     print("nothing here yet")
 
+
 def displayPrettyExpenses(queryresult):
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
     i = 1
     for date, entity, expense, tag, notes in queryresult:
         print(i, date, entity, expense, tag, notes)
-        i = i +1
+        i = i + 1
+
 
 # def writeExpenseToDB(date, charge_name, expense, tag="' '", notes="' '"):
 #     expensedata = date + "," + charge_name + "," + expense + "," + tag + "," + notes
